687b2310cd3f4125f14808a07592d1da73f3bd4a
pgherveou <pgherveou@gmail.com>
2024-11-29 12:46:21 +0100

    disable walletConnect if not specified

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -2,12 +2,15 @@ import { http, createConfig } from 'wagmi'
 import { coinbaseWallet, injected, walletConnect } from 'wagmi/connectors'
 import { AssetHubWestend, Kitchensink } from './chains'
 
+const wallectConnectId = import.meta.env.VITE_WC_PROJECT_ID
 export const config = createConfig({
     chains: [Kitchensink, AssetHubWestend],
     connectors: [
         injected(),
         coinbaseWallet(),
-        walletConnect({ projectId: import.meta.env.VITE_WC_PROJECT_ID }),
+        ...(wallectConnectId
+            ? [walletConnect({ projectId: wallectConnectId })]
+            : []),
     ],
     transports: {
         [Kitchensink.id]: http(),
