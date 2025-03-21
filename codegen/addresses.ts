98ba17ce351bcf173acd3e4522b9f58b933336d8
pgherveou <pgherveou@gmail.com>
2025-03-21 11:33:39 +0100

    rm codegen files

--- codegen/addresses.ts
+++
@@ -1,4 +0,0 @@
-
-export const WagmiMintExample = "0xeac34c0e5759c43ca877dd03aa1565e11bc534f7";
-export const Dao = "0x82726d8e34e105d82c9f4d898ffd561481486e01";
-export const DaoAttacker = "0xf633faaf68ebabe12df46c65d2d2edd43724c798";
