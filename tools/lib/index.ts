f92c3c1bc5ad4a3764f001e5f0e337eabaa4d3f1
pgherveou <pgherveou@gmail.com>
2025-10-24 12:36:26 +0200

    fix

--- tools/lib/index.ts
+++ tools/lib/index.ts
@@ -20,9 +20,18 @@ const codegenDir = join(import.meta.dirname!, '..', '..', 'codegen')
 
 const rpcUrl = Deno.env.get('RPC_URL') ?? 'http://localhost:8545'
 
-// default account is 0xf24FF3a9CF04c71Dbc94D0b566f7A27B94566cac
-const privateKey = (Deno.env.get('PRIVATE_KEY') ??
-    '0x5fb92d6e98884f76de468fa3f6278f8807c48bebc13595d45af5bdc4da702133') as Hex
+/// load private key, default account is 0xf24FF3a9CF04c71Dbc94D0b566f7A27B94566cac
+function loadPrivateKey(): Hex {
+    if (Deno.env.has('PRIVATE_KEY')) {
+        const privateKey = Deno.env.get('PRIVATE_KEY')
+        return privateKey!.startsWith('0x')
+            ? privateKey as Hex
+            : ('0x' + privateKey) as Hex
+    }
+    return '0x5fb92d6e98884f76de468fa3f6278f8807c48bebc13595d45af5bdc4da702133'
+}
+
+const privateKey = loadPrivateKey()
 
 export const env = await createEnv({
     rpcUrl,
